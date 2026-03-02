24db7ba2ea874765a53e647b4576076848dabb0b
Jhoana Mae Alvarez <jhoanamaealvarez.scc@gmail.com>
2026-03-02 13:59:56 +0800

    Update ASSIGNMENT1- PART3.java

--- ASSIGNMENT1- PART3.java
+++ ASSIGNMENT1- PART3.java
@@ -5,30 +5,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("Simple Login");
+        JFrame frame = new JFrame("Button Example");
+        JButton button = new JButton("Click Me");
 
-        JLabel label = new JLabel("Username:");
-        label.setBounds(50, 50, 100, 30);
-
-        JTextField textField = new JTextField();
-        textField.setBounds(150, 50, 150, 30);
-
-        JButton button = new JButton("Login");
-        button.setBounds(150, 100, 100, 30);
+        button.setBounds(130, 100, 100, 40);
 
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String username = textField.getText();
-                JOptionPane.showMessageDialog(frame, 
-                        "Welcome " + username);
+                JOptionPane.showMessageDialog(frame, "Button Clicked!");
             }
         });
 
-        frame.add(label);
-        frame.add(textField);
         frame.add(button);
-
-        frame.setSize(400, 250);
+        frame.setSize(400, 300);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
